979b9fdf450d2889d666f1290f0bc14c894c7379
Jordan Gelling <68049810+CurlzG@users.noreply.github.com>
2020-08-20 02:19:26 +1200

    Add files via upload

--- src/GUI.java
+++ src/GUI.java
@@ -12,6 +12,7 @@ import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -22,13 +23,28 @@ public class GUI extends JFrame implements Observer{
 
     private static final int WIDTH = 1200;
     private static final int HEIGHT = 1000;
-
+    private static Image hallway = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\yellowsquare.png").getImage();
+    private static Image wall = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\wallimage.png").getImage();
+    private static Image Ballroom = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Ballroom.png").getImage();
+    private static Image billard = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\billard.png").getImage();
+    private static Image conservatory = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Conservatory.png").getImage();
+    private static Image Kitchen = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\kitchenImage.png").getImage();
+    private static Image study = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Study.png").getImage();
+    private static Image Hall = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Hall.png").getImage();
+    private static Image dinningRoom = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Dinningroom.png").getImage();
+    private static Image door = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\doorway.png").getImage();
+    private static Image Library = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Library.png").getImage();
+    private static Image start = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Start.png").getImage();
     private Game game = null;
 
     public GUI(Game game){
         this.game = game;
 
     }
+   
+    	
+    	
+    
 
     public void display(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -37,7 +53,9 @@ public class GUI extends JFrame implements Observer{
         setTitle("Cluedo");
         setSize(WIDTH, HEIGHT);
         createMenu();
-        //createBoardArea();
+        
+        
+       // createBoardArea();
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -79,28 +97,77 @@ public class GUI extends JFrame implements Observer{
         this.game = new Game();
     }
 
-//    public void createBoardArea(){
-//        GridBagConstraints board = new GridBagConstraints();
-//        GridBagLayout gbl = new GridBagLayout();
-//        setLayout(gbl);
-//        JLabel square = new JLabel("s");
-//        square.setBorder(new LineBorder(Color.BLUE));
-//        int[] columns = new int[29];
-//        Arrays.fill(columns, 10);
-//        gbl.columnWidths = columns;
-//        int[] rows = new int[29];
-//        Arrays.fill(rows, 10);
-//        gbl.rowHeights = rows;
-//
-//        //Cell[][] currentBoard = game.getBoard().getCells();
-//        board.fill = GridBagConstraints.BOTH;
-//        //add(square, board);
-//    }
+    public void createBoardArea(){
+        GridBagConstraints board = new GridBagConstraints();
+        GridBagLayout gbl = new GridBagLayout();
+        setLayout(gbl);
+        JLabel square = new JLabel("s");
+        square.setBorder(new LineBorder(Color.BLUE));
+        int[] columns = new int[29];
+        Arrays.fill(columns, 10);
+        gbl.columnWidths = columns;
+        int[] rows = new int[29];
+        Arrays.fill(rows, 10);
+        gbl.rowHeights = rows;
+
+        Cell[][] currentBoard = game.getBoard().getCells();
+        board.fill = GridBagConstraints.BOTH;
+        add(square, board);
+    }
 
     public void addLabel(String text, GridBagConstraints board){
         JLabel label = new JLabel(text);
         label.setBorder(new LineBorder(Color.BLUE));
         add(label, board);
+    }
+    public void paint(Graphics g) {
+    	System.out.println(game.getBoard().toString());
+    	Cell[][] c = game.getBoard().getCells();
+    	Board board = new Board(c);
+    	Cell[][] cc = board.getCells();
+    	for(int i =0; i< 30; i++) {
+    		for(int j = 0;j< 30; j++) {
+    			if(cc[i][j].getType().equals(Cell.Type.HALLWAY)) {
+    				g.drawImage(hallway, 50+(i*30), 50+(j*30), 25, 25, null);
+    			} 
+    			if(cc[i][j].getType().equals(Cell.Type.WALL)) {
+    				g.drawImage(wall, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.BALLROOM)) {
+    				g.drawImage(Ballroom, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.KITCHEN)) {
+    				g.drawImage(Kitchen, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.STUDY)) {
+    				g.drawImage(study, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.CONSERVATORY)) {
+    				g.drawImage(conservatory, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.BILLIARD)) {
+    				g.drawImage(billard, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.DOOR)) {
+    				g.drawImage(door, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.DINING)) {
+    				g.drawImage(dinningRoom, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.HALL)) {
+    				g.drawImage(Hall, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.LIBRARY)) {
+    				g.drawImage(Library, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			if(cc[i][j].getType().equals(Cell.Type.START)) {
+    				g.drawImage(start, 50+(i*30), 50+(j*30), 25, 25, null);
+    			}
+    			
+    			
+    			//g.drawImage(sqaure, 50*i, 50*i, 25, 25, null);
+    		}
+    	}
     }
 
 
