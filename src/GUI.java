63b42b40780bbff9a64ca88a5162a75c3fe69ed8
msoakai <68981503+msoakai@users.noreply.github.com>
2020-08-21 16:24:07 +1200

    addingMenus/MouseActions

--- src/GUI.java
+++ src/GUI.java
@@ -19,32 +19,31 @@ import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
-public class GUI extends JFrame implements Observer{
-
+public class GUI extends JFrame implements Observer,ActionListener, MouseListener, KeyListener{
+    //private static List<Character> characters = new ArrayList<Character>();
+    private static int numPlayer;
+    private ArrayList<Character> players;
     private static final int WIDTH = 1200;
     private static final int HEIGHT = 1000;
-    private static Image hallway = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\yellowsquare.png").getImage();
-    private static Image wall = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\wallimage.png").getImage();
-    private static Image Ballroom = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Ballroom.png").getImage();
-    private static Image billard = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\billard.png").getImage();
-    private static Image conservatory = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Conservatory.png").getImage();
-    private static Image Kitchen = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\kitchenImage.png").getImage();
-    private static Image study = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Study.png").getImage();
-    private static Image Hall = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Hall.png").getImage();
-    private static Image dinningRoom = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Dinningroom.png").getImage();
-    private static Image door = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\doorway.png").getImage();
-    private static Image Library = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Library.png").getImage();
-    private static Image start = new ImageIcon("C:\\Users\\Jorda\\Desktop\\Esclipse_Files\\Cludeo_GUI\\src\\Images\\Start.png").getImage();
+    private static Image hallway = new ImageIcon("Images/yellowsquare.png").getImage();
+    private static Image wall = new ImageIcon("Images/wallimage.png").getImage();
+    private static Image Ballroom = new ImageIcon("Images/Ballroom.png").getImage();
+    private static Image billard = new ImageIcon("Images/billard.png").getImage();
+    private static Image conservatory = new ImageIcon("Images/Conservatory.png").getImage();
+    private static Image Kitchen = new ImageIcon("Images/kitchenImage.png").getImage();
+    private static Image study = new ImageIcon("Images/Study.png").getImage();
+    private static Image Hall = new ImageIcon("Images/Hall.png").getImage();
+    private static Image dinningRoom = new ImageIcon("Images/Dinningroom.png").getImage();
+    private static Image door = new ImageIcon("Images/doorway.png").getImage();
+    private static Image Library = new ImageIcon("/Images/Library.png").getImage();
+    private static Image start = new ImageIcon("/Images/Start.png").getImage();
     private Game game = null;
 
     public GUI(Game game){
         this.game = game;
+        createMenu();
 
     }
-   
-    	
-    	
-    
 
     public void display(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -52,13 +51,26 @@ public class GUI extends JFrame implements Observer{
         setLayout(new BorderLayout());
         setTitle("Cluedo");
         setSize(WIDTH, HEIGHT);
+
         createMenu();
-        
-        
-       // createBoardArea();
+        checknumPlayers();
+
+        // createBoardArea();
         setLocationRelativeTo(null);
         setVisible(true);
     }
+
+    /**
+     * Pick number of players to play  
+     */
+    public void checknumPlayers() {
+        String[] numOptions = { "2","3", "4", "5", "6" };
+        //number of players 
+        numPlayer = (int) JOptionPane.showInputDialog(null, "How many players?", "Cluedo",
+            JOptionPane.QUESTION_MESSAGE, null, numOptions, numOptions[0]);
+        // Create player list
+    }
+    // public void setPlayers() { }
 
     public void update(Observable obs, Object obj){
         if (obs instanceof Game){
@@ -69,28 +81,42 @@ public class GUI extends JFrame implements Observer{
         }
     }
 
-    private JOptionPane textPane = new JOptionPane();
-    private JButton quit = new JButton();
-    private JMenuBar menu = new JMenuBar();
-    private Container c = getContentPane();
+    // private JOptionPane textPane = new JOptionPane();
+    // private JButton quit = new JButton();
+    //  private JMenuBar menu = new JMenuBar();
+    // private Container c = getContentPane();
 
     public void createMenu(){
-        final JMenu mainMenu = new JMenu("Menu");
+        setVisible(true);       
+        JMenuBar menuBar = new JMenuBar();     
+        //panel for menu
+        JMenu turn = new JMenu("Who's Turn");
+        JMenu suggest = new JMenu("Suggestion");
+        JMenu accuse = new JMenu("Accusation");
+        JMenu reset = new JMenu("Reset");
+        //panel for submenu
+        JMenuItem subTurn = new JMenuItem("Next Player");
+        JMenuItem subSuggest = new JMenuItem("Make Suggestion");
+        JMenuItem subAccuse = new JMenuItem("Make Accusation");
+        JMenuItem reStart = new JMenuItem("Restart");       
+        //action onMouse
+        reStart.addActionListener(this);
+        subAccuse.addActionListener(this);
+        subSuggest.addActionListener(this);
+        subTurn.addActionListener(this);
+        //add submenu to menu 
+        accuse.add(subAccuse);
+        turn.add(subTurn);
+        suggest.add(subSuggest);
+        reset.add(reStart);        
+        //adding menu to  menubar
+        menuBar.add(turn);
+        menuBar.add(accuse);
+        menuBar.add(suggest);
+        menuBar.add(reset);
 
-        final JMenuItem reset = new JMenuItem("Reset");
+        this.setJMenuBar(menuBar);
 
-        menu.add(mainMenu);
-        mainMenu.add(reset);
-
-        setJMenuBar(menu);
-
-        reset.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){
-                resetGame();
-            }
-        });
-        c.add(menu, BorderLayout.NORTH);
     }
 
     public void resetGame(){
@@ -113,6 +139,7 @@ public class GUI extends JFrame implements Observer{
         Cell[][] currentBoard = game.getBoard().getCells();
         board.fill = GridBagConstraints.BOTH;
         add(square, board);
+        //Game.createWeapons();
     }
 
     public void addLabel(String text, GridBagConstraints board){
@@ -120,68 +147,132 @@ public class GUI extends JFrame implements Observer{
         label.setBorder(new LineBorder(Color.BLUE));
         add(label, board);
     }
+
     public void paint(Graphics g) {
-    	System.out.println(game.getBoard().toString());
-    	Cell[][] c = game.getBoard().getCells();
-    	Board board = new Board(c);
-    	Cell[][] cc = board.getCells();
-    	for(int i =0; i< 30; i++) {
-    		for(int j = 0;j< 30; j++) {
-    			if(cc[i][j].getType().equals(Cell.Type.HALLWAY)) {
-    				g.drawImage(hallway, 50+(i*30), 50+(j*30), 25, 25, null);
-    			} 
-    			if(cc[i][j].getType().equals(Cell.Type.WALL)) {
-    				g.drawImage(wall, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.BALLROOM)) {
-    				g.drawImage(Ballroom, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.KITCHEN)) {
-    				g.drawImage(Kitchen, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.STUDY)) {
-    				g.drawImage(study, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.CONSERVATORY)) {
-    				g.drawImage(conservatory, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.BILLIARD)) {
-    				g.drawImage(billard, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.DOOR)) {
-    				g.drawImage(door, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.DINING)) {
-    				g.drawImage(dinningRoom, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.HALL)) {
-    				g.drawImage(Hall, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.LIBRARY)) {
-    				g.drawImage(Library, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			if(cc[i][j].getType().equals(Cell.Type.START)) {
-    				g.drawImage(start, 50+(i*30), 50+(j*30), 25, 25, null);
-    			}
-    			
-    			
-    			//g.drawImage(sqaure, 50*i, 50*i, 25, 25, null);
-    		}
-    	}
+        System.out.println(game.getBoard().toString());
+        Cell[][] c = game.getBoard().getCells();
+        Board board = new Board(c);
+        Cell[][] cc = board.getCells();
+        for(int i =0; i< 30; i++) {
+            for(int j = 0;j< 30; j++) {
+                if(cc[i][j].getType().equals(Cell.Type.HALLWAY)) {
+                    g.drawImage(hallway, 50+(i*30), 50+(j*30), 25, 25, null);
+                } 
+                if(cc[i][j].getType().equals(Cell.Type.WALL)) {
+                    g.drawImage(wall, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.BALLROOM)) {
+                    g.drawImage(Ballroom, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.KITCHEN)) {
+                    g.drawImage(Kitchen, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.STUDY)) {
+                    g.drawImage(study, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.CONSERVATORY)) {
+                    g.drawImage(conservatory, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.BILLIARD)) {
+                    g.drawImage(billard, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.DOOR)) {
+                    g.drawImage(door, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.DINING)) {
+                    g.drawImage(dinningRoom, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.HALL)) {
+                    g.drawImage(Hall, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.LIBRARY)) {
+                    g.drawImage(Library, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                if(cc[i][j].getType().equals(Cell.Type.START)) {
+                    g.drawImage(start, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+
+                //g.drawImage(sqaure, 50*i, 50*i, 25, 25, null);
+            }
+        }
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Make Accusation")) {       
+            // Game.accusationMade();
+            JOptionPane.showMessageDialog(this, " SORRY !! You cannot make an accusation now");
+        }
+        if (e.getActionCommand().equals("Make Suggestion")) {
+            // Game.makeSuggestionMade();
+            JOptionPane.showMessageDialog(this, "SORRY !! You cannot make a suggestion now");
+        }
+        if (e.getActionCommand().equals("Next Player")) {
+            //GameOn();
+        }
+        if (e.getActionCommand().equals("Restart")) {
+            JOptionPane.showMessageDialog(this, "Reset loading ....");
+            resetGame();
+        }
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    /**
+     * Moving the cell
+     * 
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // movePlayer from cell to cell
+
+    }
+
+    /**
+     * key events 
+     * 
+     * @param e
+     */
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //while(!game.gameWon) {}
+    }
 
     public static void main(String[] args){
         Game game = new Game();
         GUI gui = new GUI(game);
+
         game.addObserver(gui);
         SwingUtilities.invokeLater(new Runnable() {
-            public void run(){
-                gui.display();
-            }
-        });
+                public void run(){
+                    gui.display();
+                }
+            });
 
     }
 }
-
 
