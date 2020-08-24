d142cffd751191bd90da5cfa5979d81a88d41dd8
Bryonych <49720607+Bryonych@users.noreply.github.com>
2020-08-24 21:16:08 +1200

    Add weapon images

--- src/GUI.java
+++ src/GUI.java
@@ -23,7 +23,7 @@ public class GUI extends JFrame implements Observer,ActionListener, MouseListene
     //private static List<Character> characters = new ArrayList<Character>();
     private static int numPlayer;
     private ArrayList<Character> players;
-    private static final int WIDTH = 1200;
+    private static final int WIDTH = 1400;
     private static final int HEIGHT = 1000;
     private static Image hallway = new ImageIcon("Images/yellowsquare.png").getImage();
     private static Image wall = new ImageIcon("Images/wallimage.png").getImage();
@@ -35,8 +35,15 @@ public class GUI extends JFrame implements Observer,ActionListener, MouseListene
     private static Image Hall = new ImageIcon("Images/Hall.png").getImage();
     private static Image dinningRoom = new ImageIcon("Images/Dinningroom.png").getImage();
     private static Image door = new ImageIcon("Images/doorway.png").getImage();
-    private static Image Library = new ImageIcon("/Images/Library.png").getImage();
-    private static Image start = new ImageIcon("/Images/Start.png").getImage();
+    private static Image Library = new ImageIcon("Images/Library.png").getImage();
+    private static Image lounge = new ImageIcon("Images/lounge.png").getImage();
+    private static Image start = new ImageIcon("Images/Start.png").getImage();
+    private static Image candlestick = new ImageIcon("Images/candlestick.PNG").getImage();
+    private static Image dagger = new ImageIcon("Images/dagger.PNG").getImage();
+    private static Image leadpipe = new ImageIcon("Images/leadpipe.PNG").getImage();
+    private static Image revolver = new ImageIcon("Images/revolver.PNG").getImage();
+    private static Image rope = new ImageIcon("Images/rope.PNG").getImage();
+    private static Image wrench = new ImageIcon("Images/wrench.PNG").getImage();
     private Game game = null;
 
     public GUI(Game game){
@@ -123,75 +130,72 @@ public class GUI extends JFrame implements Observer,ActionListener, MouseListene
         this.game = new Game();
     }
 
-    public void createBoardArea(){
-        GridBagConstraints board = new GridBagConstraints();
-        GridBagLayout gbl = new GridBagLayout();
-        setLayout(gbl);
-        JLabel square = new JLabel("s");
-        square.setBorder(new LineBorder(Color.BLUE));
-        int[] columns = new int[29];
-        Arrays.fill(columns, 10);
-        gbl.columnWidths = columns;
-        int[] rows = new int[29];
-        Arrays.fill(rows, 10);
-        gbl.rowHeights = rows;
 
-        Cell[][] currentBoard = game.getBoard().getCells();
-        board.fill = GridBagConstraints.BOTH;
-        add(square, board);
-        //Game.createWeapons();
-    }
-
-    public void addLabel(String text, GridBagConstraints board){
-        JLabel label = new JLabel(text);
-        label.setBorder(new LineBorder(Color.BLUE));
-        add(label, board);
-    }
 
     public void paint(Graphics g) {
-        System.out.println(game.getBoard().toString());
+
         Cell[][] c = game.getBoard().getCells();
-        Board board = new Board(c);
-        Cell[][] cc = board.getCells();
+        //Board board = new Board(c);
+        //Cell[][] cc = board.getCells();
         for(int i =0; i< 30; i++) {
             for(int j = 0;j< 30; j++) {
-                if(cc[i][j].getType().equals(Cell.Type.HALLWAY)) {
+                if(c[j][i].getType().equals(Cell.Type.HALLWAY)) {
                     g.drawImage(hallway, 50+(i*30), 50+(j*30), 25, 25, null);
                 } 
-                if(cc[i][j].getType().equals(Cell.Type.WALL)) {
+                else if(c[j][i].getType().equals(Cell.Type.WALL)) {
                     g.drawImage(wall, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.BALLROOM)) {
+                else if(c[j][i].getType().equals(Cell.Type.BALLROOM)) {
                     g.drawImage(Ballroom, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.KITCHEN)) {
+                else if(c[j][i].getType().equals(Cell.Type.KITCHEN)) {
                     g.drawImage(Kitchen, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.STUDY)) {
+                else if(c[j][i].getType().equals(Cell.Type.STUDY)) {
                     g.drawImage(study, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.CONSERVATORY)) {
+                else if(c[j][i].getType().equals(Cell.Type.CONSERVATORY)) {
                     g.drawImage(conservatory, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.BILLIARD)) {
+                else if(c[j][i].getType().equals(Cell.Type.BILLIARD)) {
                     g.drawImage(billard, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.DOOR)) {
+                else if(c[j][i].getType().equals(Cell.Type.DOOR)) {
                     g.drawImage(door, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.DINING)) {
+                else if(c[j][i].getType().equals(Cell.Type.DINING)) {
                     g.drawImage(dinningRoom, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.HALL)) {
+                else if(c[j][i].getType().equals(Cell.Type.HALL)) {
                     g.drawImage(Hall, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.LIBRARY)) {
+                else if(c[j][i].getType().equals(Cell.Type.LIBRARY)) {
                     g.drawImage(Library, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-                if(cc[i][j].getType().equals(Cell.Type.START)) {
+                else if(c[j][i].getType().equals(Cell.Type.START)) {
                     g.drawImage(start, 50+(i*30), 50+(j*30), 25, 25, null);
                 }
-
+                else if(c[j][i].getType().equals(Cell.Type.LOUNGE)) {
+                    g.drawImage(lounge, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                else if(c[j][i].getType().equals(Cell.Type.CANDLESTICK)) {
+                    g.drawImage(candlestick, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                else if(c[j][i].getType().equals(Cell.Type.DAGGER)) {
+                    g.drawImage(dagger, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                else if(c[j][i].getType().equals(Cell.Type.LEADPIPE)) {
+                    g.drawImage(leadpipe, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                else if(c[j][i].getType().equals(Cell.Type.REVOLVER)) {
+                    g.drawImage(revolver, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                else if(c[j][i].getType().equals(Cell.Type.ROPE)) {
+                    g.drawImage(rope, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
+                else if(c[j][i].getType().equals(Cell.Type.SPANNER)) {
+                    g.drawImage(wrench, 50+(i*30), 50+(j*30), 25, 25, null);
+                }
                 //g.drawImage(sqaure, 50*i, 50*i, 25, 25, null);
             }
         }
